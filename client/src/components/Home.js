f653d06c142d986b7c36b1b796da5f5d416febd3
mac718 <mac718@gmail.com>
2020-01-27 20:31:22 -0800

    create Home component

---
+++ client/src/components/Home.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react'
+
+class Home extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      message: 'loading...'
+    }
+  }
+
+  componentDidMount() {
+    fetch('/home')
+      .then(res => res.json())
+      .then(json => this.setState({message: json.message}))
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Home</h1>
+        <p>{this.state.message}</p>
+      </div>
+    )
+  }
+
+}
+
+export default Home;
