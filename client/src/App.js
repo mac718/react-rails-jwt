ea5b3a99b9953fb0eb95c8a3defa139536572d4e
mac718 <mac718@gmail.com>
2020-01-28 08:25:08 -0800

    create Login component

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './components/Home';
 import Secret from './components/Secret';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <Home />
       <Secret />
+      <Login />
     </div>
   );
 }
---
+++ client/src/components/Login.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  handleInputChange = (e) => {
+    const { value, name } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('api/authenticate', {
+      method: 'POST',
+      body: JSON.stringify(this.state),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+      .then(res => {
+        if(res.status === 200) {
+          this.props.history.push('/');
+        } else {
+          const error = new Error(res.error)
+          throw error;
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        alert('Error logging in');
+      })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h1>Login below!</h1>
+          <div className='field column is-one-third is-offset-one-third' >
+            <div className='control'>
+              <input 
+                className='input'
+                type='email'
+                name='email'
+                placeholder='enter email'
+                value={this.state.email}
+                onChange={this.handleInputChange}
+                required
+              />
+            </div>
+          </div>
+          <div className='field column is-one-third is-offset-one-third' >
+            <div className='control'>
+              <input 
+                className='input'
+                type='password'
+                name='password'
+                placeholder='enter password'
+                value={this.state.password}
+                onChange={this.handleInputChange}
+                required
+              />
+            </div>
+          </div>
+            <input className='button is-primary' type='submit' value='Submit' />
+      </form>
+    );
+  }
+}
+
+export default Login;
